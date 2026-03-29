6d078f28324f650b6388733dab38da561793cb79
nhatle1909 <nle75234@gmail.com>
2026-03-29 20:30:54 +0700

    update : NoteControl - improve performance

--- WPF_Piano/ViewModel/MainViewVM.cs
+++ WPF_Piano/ViewModel/MainViewVM.cs
@@ -2,13 +2,18 @@
 {
     public class MainViewVM
     {
-        public PianoButtonVM PianoButtonVM;
-        public SongPlayerVM SongPlayerVM;
-
+        public PianoButtonVM PianoButtonVM { get; set; }
+        public SongPlayerVM SongPlayerVM { get; set; }
+        public SongVM SongVM { get; set; } 
         public MainViewVM()
         {
             PianoButtonVM = new PianoButtonVM();
+            SongPlayerVM = new SongPlayerVM();
+            SongVM = new SongVM();
+            SongVM.SongSelected += (song) =>
+            {
+                SongPlayerVM.LoadMidiFile(song.FilePath);
+            };
         }
-
     }
 }
